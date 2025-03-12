994c2776d7063447b0c1a87a4dcd28f38fc235bd
giriaakula <giriakula3@gmail.com>
2025-03-12 15:36:14 +0530

    six

--- giri.sh
+++ giri.sh
@@ -1,3 +1,3 @@
 echo "Creating service...."
-SERVICE=$(curl --location --request POST https://kong-ola-apigateway-oem-8001a.evkcs.corp.olaelectric.com/services --header 'Content-Type: application/x-www-form-urlencoded' --header "KAT: 0729a611-74e0-4ffe-9404-1fffe412cae2" --data-urlencode 'name=blaze-coco5S' --data-urlencode 'url=http://blaze-18000.int.lb')
+SERVICE=$(curl --location --request POST http://kong-ola-apigateway-oem-8001a.evkcs.corp.olaelectric.com/services --header 'Content-Type: application/x-www-form-urlencoded' --header "KAT: 0729a611-74e0-4ffe-9404-1fffe412cae2" --data-urlencode 'name=blaze-coco5S' --data-urlencode 'url=http://blaze-18000.int.lb')
 echo $SERVICE
